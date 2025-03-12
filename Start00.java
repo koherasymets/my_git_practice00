573169e71f430b3f3ebbe23163dfeefdea3ecde8
Kostiantyn Herasymets <ko.herasymets@gmail.com>
2025-03-12 22:09:08 +0100

    Initial commit add "NO WAR"

--- Start00.java
+++ Start00.java
@@ -1,6 +1,6 @@
 public class Start00 {
 
     public static void main(String[] args) {
-        System.out.println("!Hello World! Peace");
+        System.out.println("!Hello World! Peace, NO WAR");
     }
 }
